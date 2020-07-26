ca3cb57d5b0a5ef3b1ade58f07ee7dbbaac7ce06
seneark <seneark@protonmail.com>
2020-07-26 13:19:35 +0530

    added Payment Feature

--- backend/jwa/paytm/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  MID: 'qlgPPu13461450341249',
-  PAYTM_MERCHANT_KEY: '@EMjpi62b51Z#CAj',
-  PAYTM_FINAL_URL: 'https://securegw-stage.paytm.in/theia/processTransaction',
-  WEBSITE: 'WEBSTAGING',
-  CHANNEL_ID: 'WEB',
-  INDUSTRY_TYPE_ID: 'Retail',
-  CALLBACK_URL: 'http://localhost:3000/pay/paywithpaytmresponse'
-};
